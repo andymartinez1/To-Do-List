7fce28ce80f971fe5e29de35cef50172de4ae87e
Andrew Martinez <martinez.s.andrew@gmail.com>
2025-05-13 13:35:17 -0400

    Add project files.

---
+++ ToDoList/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("What would you like to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd new TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+Console.WriteLine("User input: " + userChoice);
+
+if (userChoice == "S")
+{
+    PrintSelectedOption("See all TODOs");
+}
+
+else if (userChoice == "A")
+{
+    PrintSelectedOption("Add new TODO");
+}
+
+else if (userChoice == "R")
+{
+    PrintSelectedOption("Remove a TODO");
+}
+
+else if (userChoice == "E")
+{
+    PrintSelectedOption("Exit");
+}
+
+Console.ReadKey();
+
+void PrintSelectedOption(string selectedOption)
+{
+    Console.WriteLine("Selected option: " + selectedOption);
+}
