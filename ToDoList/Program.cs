f90c315d5e067e6362f5208c865d688ea7ff8803
Andrew Martinez <andy72391@gmail.com>
2025-05-14 11:17:11 -0400

    Added functionality based on the user's inputs. Users can add, remove or view all tasks.

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,36 +1,117 @@
-﻿Console.WriteLine("Hello!");
-Console.WriteLine("What would you like to do?");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd new TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+﻿var todoList = new List<string>();
 
-string userChoice = Console.ReadLine();
-Console.WriteLine("User input: " + userChoice);
+Console.WriteLine("Hello!");
 
-if (userChoice == "S")
+
+bool exitProgram = false;
+while (!exitProgram)
 {
-    PrintSelectedOption("See all TODOs");
+    Console.WriteLine();
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd new TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+
+
+    string userChoice = Console.ReadLine();
+    Console.WriteLine();
+
+    switch (userChoice)
+    {
+        case "s":
+        case "S":
+            SeeAllTodos();
+            break;
+        case "a":
+        case "A":
+            AddTodo();
+            break;
+        case "r":
+        case "R":
+            RemoveTodo();
+            break;
+        case "e":
+        case "E":
+            exitProgram = true;
+            break;
+        default:
+            Console.WriteLine ("Invalid option. Please try again.");
+            break;
+    }
 }
 
-else if (userChoice == "A")
+void AddTodo()
 {
-    PrintSelectedOption("Add new TODO");
+    bool isInputValid = false;
+    while (!isInputValid)
+    {
+
+
+        Console.WriteLine("Enter the TODO description:");
+        string newTodo = Console.ReadLine();
+        if (newTodo == "")
+        {
+            Console.WriteLine("The description cannot be empty");
+        }
+        else if (todoList.Contains(newTodo))
+        {
+            Console.WriteLine("TODO already exists.");
+        }
+        else
+        {
+            isInputValid = true;
+            todoList.Add(newTodo);
+            Console.WriteLine("TODO added successfully.");
+        }
+    }
 }
 
-else if (userChoice == "R")
+void SeeAllTodos()
 {
-    PrintSelectedOption("Remove a TODO");
+    if(todoList.Count == 0)
+    {
+        Console.WriteLine("No TODOs have been added yet");
+    }
+    else
+    {
+        for (int i = 0; i < todoList.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {todoList[i]}");
+        }
+    }
 }
 
-else if (userChoice == "E")
+void RemoveTodo()
 {
-    PrintSelectedOption("Exit");
-}
+    if(todoList.Count == 0)
+    {
+        Console.WriteLine("No TODOs to remove.");
+        return;
+    }
 
-Console.ReadKey();
-
-void PrintSelectedOption(string selectedOption)
-{
-    Console.WriteLine("Selected option: " + selectedOption);
+    bool isInputValid = false;
+    while (!isInputValid)
+    {
+        Console.WriteLine("Select the TODO to remove: ");
+        SeeAllTodos();
+        var userInput = Console.ReadLine();
+        if(userInput == "")
+        {
+            Console.WriteLine("You must select a valid TODO");
+            continue;
+        }
+        if (int.TryParse(userInput, out int todoIndex) && todoIndex > 0 && todoIndex <= todoList.Count)
+        {
+            var indexOfTodo = todoIndex - 1;
+            var todoToBeRemoved = todoList[indexOfTodo];
+            todoList.RemoveAt(indexOfTodo);
+            Console.WriteLine("TODO removed: " + todoToBeRemoved);
+            isInputValid = true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid selection. Please try again.");
+        }
+    }
 }
